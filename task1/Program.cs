01ec5e08e40fe4a519b8991662c7726b78d13fa8
Nikita <nikitya39@gmail.com>
2022-04-04 02:24:49 +0200

    task1

---
+++ task1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.WriteLine("Введите число");
+string numbers = Convert.ToString(Console.ReadLine());
+string[] array = numbers.Split(' ');
+int[] intArray = new int[array.Length];
+for (int i = 0; i < array.Length; i++)
+{
+    intArray[i] = Convert.ToInt32(array[i]);
+    Console.WriteLine(intArray[i]);
+}
+ int count = 0;
+ for (int i = 0; i < intArray.Length; i++ )
+ {
+     if(intArray[i] > 0)
+     {
+         count = count + 1;
+     }
+ }
+ Console.WriteLine (count);
